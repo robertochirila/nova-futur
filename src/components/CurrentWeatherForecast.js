7674b0b93e132ddad55b3283fbaedcc87c7dc847
Roberto <robertochirila@yahoo.com>
2020-09-16 14:50:28 +0100

    added hook and interval function

--- src/components/CurrentWeatherForecast.js
+++ src/components/CurrentWeatherForecast.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export const CurrentWeatherForecast = (props) => {
     const { currentWeather } = props;
-    console.log(currentWeather);
-    return <div></div>;
+    const [progressValue, setValue] = useState(0);
+    var value = 0;
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            console.log("This will run every second!");
+            setValue((progressValue) => progressValue + 1);
+            /*if (progressValue > 60) {
+                setValue((progressValue) => (progressValue = 0));
+            }*/
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+    console.log(progressValue);
+    // trigger the event in App js that will refresh the data
+    return (
+        <div>
+            <label htmlFor="file">Downloading progress:</label>
+            <progress id="file" value={progressValue} max="60">
+                {progressValue}%
+            </progress>
+        </div>
+    );
 };
