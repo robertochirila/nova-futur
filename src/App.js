13d1ef75c38df857e626944feb1d35d15fa08772
Roberto <robertochirila@yahoo.com>
2020-09-17 13:21:43 +0100

    fixed refresh bug

--- src/App.js
+++ src/App.js
@@ -8,62 +8,62 @@ export class App extends Component {
         this.state = {
             currentWeather: [],
             nextDaysWeather: [],
-            step: 0,
+            step: true,
         };
     }
+
     componentDidMount() {
+        console.count("how many times");
+        this.fetchData();
+    }
+
+    fetchData = () => {
         const { step } = this.state;
         var currentWeather;
         var nextDaysWeather;
-        if (step === 0) {
-            fetch(
-                "https://api.openweathermap.org/data/2.5/weather?q=London&appid=1fc71092a81b329e8ce0e1ae88ef0fb7"
-            )
-                .then((response) => {
-                    const contentType = response.headers.get("content-type");
-                    if (
-                        !contentType ||
-                        !contentType.includes("application/json")
-                    ) {
-                        throw new TypeError("No JSON data!");
-                    }
-                    return response.json();
-                })
-                .then((data) => {
-                    currentWeather = data;
-                })
-                .catch((error) => console.error(error));
-            fetch(
-                "https://api.openweathermap.org/data/2.5/forecast?q=London&appid=1fc71092a81b329e8ce0e1ae88ef0fb7"
-            )
-                .then((response) => {
-                    const contentType = response.headers.get("content-type");
-                    if (
-                        !contentType ||
-                        !contentType.includes("application/json")
-                    ) {
-                        throw new TypeError("No JSON data!");
-                    }
-                    return response.json();
-                })
-                .then((data) => {
-                    let requiredData = data.list.slice(0, 5);
-                    nextDaysWeather = requiredData;
-                })
-                .catch((error) => console.error(error));
-            let f = setTimeout(() => {
-                this.setState({
-                    currentWeather: currentWeather,
-                    nextDaysWeather: nextDaysWeather,
-                    step: 1,
-                });
-            }, 1000);
-        }
-    }
+
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=London&appid=1fc71092a81b329e8ce0e1ae88ef0fb7"
+        )
+            .then((response) => {
+                const contentType = response.headers.get("content-type");
+                if (!contentType || !contentType.includes("application/json")) {
+                    throw new TypeError("No JSON data!");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                currentWeather = data;
+            })
+            .catch((error) => console.error(error));
+        fetch(
+            "https://api.openweathermap.org/data/2.5/forecast?q=London&appid=1fc71092a81b329e8ce0e1ae88ef0fb7"
+        )
+            .then((response) => {
+                const contentType = response.headers.get("content-type");
+                if (!contentType || !contentType.includes("application/json")) {
+                    throw new TypeError("No JSON data!");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                let requiredData = data.list.slice(0, 5);
+                nextDaysWeather = requiredData;
+            })
+            .catch((error) => console.error(error));
+
+        let f = setTimeout(() => {
+            console.log(currentWeather);
+            console.log(nextDaysWeather);
+            this.setState({
+                currentWeather: currentWeather,
+                nextDaysWeather: nextDaysWeather,
+            });
+        }, 1000);
+    };
 
     handleRefresh = () => {
-        const { step } = this.state;
-        this.setState({ step: 0 });
+        this.fetchData();
     };
 
     render() {
--- src/components/CurrentWeatherForecast.js
+++ src/components/CurrentWeatherForecast.js
@@ -1,20 +1,45 @@
 import React, { useEffect, useState } from "react";
 
-export const CurrentWeatherForecast = (props) => {
+/*export const CurrentWeatherForecast = (props) => {
     const { currentWeather } = props;
     const [progressValue, setValue] = useState(0);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setValue((progressValue) =>
-                progressValue < 6 ? progressValue + 1 : (progressValue = 0)
-            );
+            setValue((progressValue) => {
+                if (progressValue < 5) return progressValue + 1;
+                else {
+                    props.handleRefresh();
+                    return 0;
+                }
+            });
         }, 1000);
         return () => clearInterval(interval);
     }, []);
-    if (progressValue === 5) {
-        props.handleRefresh();
-    }
+
+    return (
+        <div>
+            <label htmlFor="file">Downloading progress:</label>
+            <progress id="file" value={progressValue} max="60">
+                {progressValue}%
+            </progress>
+        </div>
+    );
+};*/
+
+export const CurrentWeatherForecast = (props) => {
+    const [progressValue, setValue] = useState(0);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            props.handleRefresh();
+        }, 10000);
+        const updateProgressBar = setInterval(() => {
+            setValue((progressValue) =>
+                progressValue < 61 ? progressValue + 1 : (progressValue = 0)
+            );
+        }, 1000);
+        return () => clearInterval(interval, updateProgressBar);
+    }, []);
 
     return (
         <div>
